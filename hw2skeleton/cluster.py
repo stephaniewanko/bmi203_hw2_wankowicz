e5269bd1de083517707a467c111458164b969f4a
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-02-08 15:15:12 -0800

    Update cluster.py
    adding partitioning algo

--- hw2skeleton/cluster.py
+++ hw2skeleton/cluster.py
@@ -54,76 +54,83 @@ def similarity_metric(file_location): ### PUT THIS ALL TOGETHER
 
 ##################PARTITIONING ALGO################################
 
-#seperate functions
-def select_initial_values(dist_list, k): #k is the number of clusters #figure out how to re-iterate over this for k_i
-    k1=float(list(dist_list)[0]) #assuming this value is stored in pickle keys
+import os
+import pandas as pd
+import pickle
+import math
+import numpy as np
+
+'''
+I have set up this function to take in a dictionary file which has keys==the volume similarity metric
+(or some numerical representation of the similarity between PDBs) and values==PDB name.
+
+Initially find 3 starting points to start as the cluster means. We are choosing the first three items in the list.
+We then iterate through all items in the list, determine the square root distance away from each cluster mean.
+For each item, we assign it to the cluster that it is closest to the mean.
+Once everything has been assigned, we find a new mean for that cluster.
+
+
+'''
+
+
+def select_initial_values(dist_list): #k is the number of clusters #figure out how to re-iterate over this for k_i
+    '''This function is finding & stating the initial mean values we will have for kmeans.
+    We are using the first three items in the dictionary.
+    Input: dictionary with PDB=sim_metric
+    Output: 3 Initial values
+    '''
+    k1=float(list(dist_list)[0]) #grabbing the similarity metric from the keys in the dictionary
     k2=float(list(dist_list)[1])
     k3=float(list(dist_list)[2])
     print('k1,2,3:')
     print(k1, k2, k3)
     return k1,k2,k3
 
+
 def assign_new_clusters(dist_list,k1,k2,k3):
-    print('k1')
-    print(k1)
-    c_1=[k1]
-    c_2=[k2]
-    c_3=[k3]
-    for i in range(len(dist_list)-1): #deal with length of pickle
-        i=i+1
-        dist_k1=math.sqrt((float(list(dist_list)[i])-k1)**2) ##sqrt distance
+    '''This funciton will be used once we iterate over all the items in the entire list, assign them to a cluster and then get the mean of the newly created cluster.
+    Input: dictionary with PDB=sim_metric, 3 mean values for each cluster
+    Output: 3 means for new cluster, list of 3 clusters
+
+    '''
+    c_1=[] #creating each cluster
+    c_2=[]
+    c_3=[]
+    for i in range(len(dist_list)-1): #for length of the similarity metric,
+        i=i+1 # i is going to grab the next item in the list
+        dist_k1=math.sqrt((float(list(dist_list)[i])-k1)**2) ##sqrt distance of each item in the list from the median of each cluster
         dist_k2=math.sqrt((float(list(dist_list)[i])-k2)**2)
         dist_k3=math.sqrt((float(list(dist_list)[i])-k3)**2)
-        min_dist=min(dist_k1,dist_k2,dist_k3)
-        #print(min_dist)
+        min_dist=min(dist_k1,dist_k2,dist_k3) #for i, determine what cluster mean it is closest to
+        #assign each value to the cluster it belongs to (based on the distance)
         if min_dist==dist_k1:
             c_1.append(float(list(dist_list)[i]))#assign to c_1
         elif min_dist==dist_k2:
             c_2.append(float(list(dist_list)[i]))#assign to c_1
         else:
-            c_3.append(float(list(dist_list)[i]))
-    k1=np.median(c_1)
+            c_3.append(float(list(dist_list)[i]))#assign to c_3
+    k1=np.median(c_1) #find the median for each cluster
     k2=np.median(c_2)
     k3=np.median(c_3)
-    print('K1')
-    print(k1)
-    return k1,k2,k3,c_1,c_2,c_3
+    return k1,k2,k3,c_1,c_2,c_3 #return the clusters and the new medians & DO IT AGAIN!
 
-##MAIN FUNCTION##
-def k_means(pickle_input, k):
-    k1,k2,k3=select_initial_values(pickle_input,k) #figure out input/output
-    for i in range(100): #iterate until ?
-    #while k1_new not equal to k1...  #confidence metric #need to output name with distance metric
+def k_median(pickle_input):
+    k1,k2,k3=select_initial_values(pickle_input) #figure out input/output
+    for i in range(10): #iterate until ?
         k1,k2,k3,c_1,c_2,c_3=assign_new_clusters(pickle_input,k1,k2,k3)
-    print('c_1')
-    print(c_1)
     return c_1, c_2, c_3
 
-    
-
-def cluster_by_partitioning(active_sites):
-    """
-    Cluster a given set of ActiveSite instances using a partitioning method.
-
-    Input: a list of ActiveSite instances
-    Output: a clustering of ActiveSite instances
-            (this is really a list of clusters, each of which is list of
-            ActiveSite instances)
-    """
-    # Fill in your code here!
-
-    return 
-
-
-def cluster_hierarchically(active_sites):
-    """
-    Cluster the given set of ActiveSite instances using a hierarchical algorithm.                                                                  #
-
-    Input: a list of ActiveSite instances
-    Output: a list of clusterings
-            (each clustering is a list of lists of Sequence objects)
-    """
-
-    # Fill in your code here!
-
-    return []
+'''
+All of the clustering above was done on the similarity metrics
+'''
+#connect values back to pdb ids
+def replace_values(cluster, pickle_input):
+    new_list = []
+    for i in cluster:
+        if isinstance(i, list):
+              new_list.append(replace_vals(i, pickle_input))
+        elif i in pickle_input:
+            new_list.append(pickle_input.get(i))
+        else:
+            new_list.append(i)
+    return new_list
