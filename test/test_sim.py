398e7cab6205393cff1455f8d3584e7cdcabf27a
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-02-08 23:35:59 -0800

    Update test_sim.py

--- test/test_sim.py
+++ test/test_sim.py
@@ -12,7 +12,7 @@ def test_similarity(location):
     '''
     We are going to run the similarity algorithm, and then make sure that the similarity metric for pdb 47023 is equal to 21.028781603576693'.
     '''
-    sim_dictionary=cluster.similarity_metric(location) #make sure this is returning a dictionary
+    sim_dictionary=similarity_metric(location) #make sure this is returning a dictionary
     for key, value in sim_dict.items():
         if value=='47023.pdb':
             key_key=key
@@ -28,15 +28,15 @@ def test_partition_clustering(similarity_dictionary):
     We have set the random.seed() to 40 to ensure we get the same clusters.
     '''
 
-    cluster1,cluster2,cluster3=cluster.k_median(similarity_dictionary)
+    cluster1,cluster2,cluster3=k_median(similarity_dictionary)
     assert len(cluster1) == 10
     
-def test_hierarchical_clustering():
+def test_hierarchical_clustering(content):
     '''
     To test the hierarchical algorithm, we are going to run the full algorithm. We are then going to assess the length of the third cluster.
     We have set the random.seed() to 40 to ensure we get the same clusters.
     '''
-    test_cluster=cluster.hier_cluster(content,135)
+    test_cluster=hier_cluster(content,135)
     assert len(test_cluster) == 2
 
 
@@ -46,7 +46,7 @@ test_similarity('./data')
 
 #running the similarity metric to get the dictionary
 test_partition_clustering(content)
-
+test_hierarchical_clustering(content)
 
 
 
