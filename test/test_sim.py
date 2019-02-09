3c912c44a16cb3c9425bb8132ddc857cfc235195
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-02-09 00:31:29 -0800

    Update test_sim.py

--- test/test_sim.py
+++ test/test_sim.py
@@ -12,7 +12,7 @@ sys.path.append('data/')
 content=similarity_metric()
 print(content)
 print('Testing Similarity')
-
+content = pickle.load(open('PDB_HW2.pickle', "rb"))
 def test_similarity():
     '''
     We are going to run the similarity algorithm, and then make sure that the similarity metric for pdb 47023 is equal to 21.028781603576693'.
@@ -31,7 +31,7 @@ def test_partition_clustering():
     To test the parition algorithm, we are going to run the k-medians algorithm. We are then going to assess the length of the third cluster.
     We have set the random.seed() to 40 to ensure we get the same clusters.
     '''
-    cluster1,cluster2,cluster3=k_median('PDB_HW2.pickle')
+    cluster1,cluster2,cluster3=k_median(content)
     assert len(cluster1) == 10
     
 def test_hierarchical_clustering():
@@ -39,7 +39,7 @@ def test_hierarchical_clustering():
     To test the hierarchical algorithm, we are going to run the full algorithm. We are then going to assess the length of the third cluster.
     We have set the random.seed() to 40 to ensure we get the same clusters.
     '''
-    test_cluster=hier_cluster('PDB_HW2.pickle',135)
+    test_cluster=hier_cluster(content,135)
     assert len(test_cluster) == 2
 
 
