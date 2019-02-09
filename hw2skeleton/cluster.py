e0c7d7efd60703b206a8f15cb43e44f5410781c6
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-02-08 23:19:02 -0800

    Update cluster.py

--- hw2skeleton/cluster.py
+++ hw2skeleton/cluster.py
@@ -268,22 +268,11 @@ def return_cluster(distance_dict, num_clusters, pickle_input):
     return pdb_names, cluster
 
 ###comparison functions###
-def jacard_index(cluster1, cluster2):
-    '''
-    To compare the two clustering algorithms, we are using the Jacard Index, which determines the length of the union of the two clusters over the intersection.
-    We are going to the three cluster level.
-    The higher the Jacard index, the better the clusters compare (more intersection per item in the two indexes).
-
-    Input: one representative cluster from each algorithm
-    Output: Jacard Index
-    '''
-    union = list(set(cluster1).union(cluster2))
-    intersection = list(set(cluster1) & set(cluster2))
-    return len(intersection)/len(union)
-
-
 def silhouette(cluster1,cluster2):
     '''
+    The silhouette score compares how similar each item is to all the other items in the cluster it belongs to compared to another cluster. 
+    INPUT: 3 clusters as lists
+    OUTPUT: Silhouette Score
     '''
     mean_cluster1=mean(cluster1) #we want to find the closest cluster to compare this to
     mean_cluster2=mean(cluster2)
@@ -300,4 +289,18 @@ def silhouette(cluster1,cluster2):
             a_list.append(math.mean(sqrt((j-i)**2))) #finding the eucledian distance between each values in the same cluster
         for j in cluster2:
             b_list.append(math.mean(sqrt((j-i)**2))) #finding the eucledian distance between each values in the other cluster
+     s=(mean(a_list)-mean(b_list))/(min(a_list)-min(b_list)
+     return s 
 
+def jacard_index(cluster1, cluster2):
+    '''
+    To compare the two clustering algorithms, we are using the Jacard Index, which determines the length of the union of the two clusters over the intersection.
+    We are going to the three cluster level.
+    The higher the Jacard index, the better the clusters compare (more intersection per item in the two indexes).
+
+    Input: one representative cluster from each algorithm
+    Output: Jacard Index
+    '''
+    union = list(set(cluster1).union(cluster2))
+    intersection = list(set(cluster1) & set(cluster2))
+    return len(intersection)/len(union)
