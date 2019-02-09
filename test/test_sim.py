783a74a4347069fc196f870d9776f9e3bb2d5671
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-02-08 23:31:31 -0800

    Update test_sim.py

--- test/test_sim.py
+++ test/test_sim.py
@@ -1,5 +1,3 @@
-#pip freeze > requirements.txt
-
 from hw2skeleton import *
 from hw2skeleton import io
 import sys
@@ -32,6 +30,14 @@ def test_partition_clustering(similarity_dictionary):
 
     cluster1,cluster2,cluster3=cluster.k_median(similarity_dictionary)
     assert len(cluster1) == 10
+    
+def test_hierarchical_clustering():
+    '''
+    To test the hierarchical algorithm, we are going to run the full algorithm. We are then going to assess the length of the third cluster.
+    We have set the random.seed() to 40 to ensure we get the same clusters.
+    '''
+    test_cluster=cluster.hier_cluster(content,135)
+    assert len(test_cluster) == 2
 
 
 content=cluster.similarity_metric('./data/')
@@ -41,15 +47,7 @@ test_similarity('./data')
 #running the similarity metric to get the dictionary
 test_partition_clustering(content)
 
-def test_hierarchical_clustering():
-    '''
-    To test the hierarchical algorithm, we are going to run the full algorithm. We are then going to assess the length of the third cluster.
-    We have set the random.seed() to 40 to ensure we get the same clusters.
-    '''
-
-    assert cluster.cluster_hierarchically(active_sites) == []
 
 
-similarity_metric('./data/')
 
 assert [1]==[1]
